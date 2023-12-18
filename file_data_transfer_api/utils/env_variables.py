eb702be03752c859a154b2b901ef29153adf7338
Marta <marta.wolinska16@imperial.ac.uk>
2023-12-18 15:50:46 +0100

    bug: make environment variables not stat

--- file_data_transfer_api/utils/env_variables.py
+++ file_data_transfer_api/utils/env_variables.py
@@ -1,4 +1,15 @@
+import os
+from dataclasses import dataclass, asdict
 from pathlib import Path
 
-UPLOAD_PATH = Path(__file__).parent.parent.parent / "uploads"
-DATABASE_PATH = Path(__file__).parent.parent.parent / "image_database.json"
+UPLOAD_PATH = \
+    os.getenv(
+        "UPLOAD_PATH",
+        str(Path(__file__).parent.parent.parent / "uploads"),
+    )
+
+DATABASE_PATH = \
+    os.getenv(
+        "UPLOAD_PATH",
+        str(Path(__file__).parent.parent.parent / "image_database.json"),
+    )
